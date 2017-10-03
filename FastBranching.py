19de5920f18356483eeca3382ed16ba485064ea9
Leo <leonid.braginsky@gmail.com>
2017-10-03 15:55:46 -0400

    Added more guy controls.

--- FastBranching.py
+++ FastBranching.py
@@ -8,16 +8,37 @@ import numpy as np
 
 class Branching(object):
 
+    @staticmethod
+    def sparsity_fitness(gen, diameter):
+        from scipy import spatial
+        tree = spatial.cKDTree(gen)
+        f = lambda p: len(tree.query_ball_point(p, r=0.5*diameter))
+        return np.apply_along_axis(f, 1, gen)
+
+    fitness_functions = {
+        "euclidean":    lambda gen: np.sqrt(np.sum(gen**2, axis=1)),
+        "sumsq":        lambda gen: np.sum(gen**2, axis=1),
+        "sumabs":       lambda gen: np.sum(abs(gen), axis=1),
+        "maxabs":       lambda gen: np.max(abs(gen), axis=1),
+        "absprod":      lambda gen: np.prod(abs(gen), axis=1),
+        "sparsity(100)": (lambda gen: Branching.sparsity_fitness(gen, 100), True)
+        }
+
     def __init__(self, branch_prob, fitness, initial_population, max_size):
         self.branch_prob = branch_prob
-        try:
-            self.fitness, self.low_fit = fitness
-        except TypeError:
-            self.fitness, self.low_fit = fitness, False
+        self.set_fitness(fitness)
         self.max_size = max_size
         self.members = initial_population
         self.generation = 0
 
+    def set_fitness(self, fitness):
+        f = Branching.fitness_functions[fitness]
+        try:
+            self.fitness_fun, self.low_fit = f
+        except TypeError:
+            self.fitness_fun, self.low_fit = f, False
+        self.fitness = fitness
+        
     def step(self):
         # Everybody moves
         gen = self.members + np.random.normal(size=self.members.shape)
@@ -25,7 +46,7 @@ class Branching(object):
         ind = np.random.uniform(size=gen.shape[0]) < self.branch_prob
         gen = np.append(gen, gen[ind], axis=0)
         # Sort by fitness
-        ind = np.argsort(self.fitness(gen))
+        ind = np.argsort(self.fitness_fun(gen))
         if not self.low_fit: ind = ind[::-1]
         # Truncate to max size
         # New generation becomes members
@@ -37,117 +58,125 @@ class Branching(object):
             self.step()
 
     def stats(self):
-        f = self.fitness(self.members)
+        f = self.fitness_fun(self.members)
         return {"gen": self.generation, "count": len(f), "min": np.min(f), "max": np.max(f), "avg": np.mean(f), "std": np.std(f)}
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 
-def simulation(br, steps_per_update):
+class Simulation(object):
 
-    ndim = br.members.shape[1]
-    if not ndim in [1, 2, 3]:
-        raise NotImplementedError("Cannot display {}-dimensions".format(ndim))
-
-    ax_lims = [(-50, 50)] * ndim
-    def scaling():
-        d_lims = list(zip(np.min(br.members, axis=0), np.max(br.members, axis=0)))
-        if any(d[0] < a[0] or d[1] > a[1] for a, d in zip(ax_lims, d_lims)):
+    def __init__(self, branching, steps_per_update=1):
+        ndim = branching.members.shape[1]
+        if not ndim in [1, 2, 3]:
+            raise NotImplementedError("Cannot display {}-dimensions".format(ndim))
+        self.branching = branching
+        self.steps_per_update = steps_per_update
+        self.ndim = ndim
+        self.running = False
+        self.ax_lims = [(-50, 50)] * ndim
+        self.scatter = [self.scatter_d1, self.scatter_d2, self.scatter_d3][ndim-1]
+        self.fig = plt.figure(figsize=(9, 8))
+        self.ax = self.plot()
+        self.setup_controls()
+    
+    def scaling(self):
+        members = self.branching.members
+        d_lims = list(zip(np.min(members, axis=0), np.max(members, axis=0)))
+        if any(d[0] < a[0] or d[1] > a[1] for a, d in zip(self.ax_lims, d_lims)):
             r = max(b - a for a, b in d_lims) * 0.75
-            for d in range(ndim):
+            for d in range(len(d_lims)):
                 c = sum(d_lims[d])/2
-                ax_lims[d] = (c - r, c + r)
+                self.ax_lims[d] = (c - r, c + r)
         
-    def plot():
-        ax = fig.add_subplot(111, projection='3d' if ndim==3 else None)
+    def plot(self):
+        ax = self.fig.add_subplot(111, projection='3d' if ndim==3 else None)
         ax.set_autoscale_on(False)
         if ndim==1: ax.get_yaxis().set_visible(False)
         return ax
 
-    def scatter_d1():
-        x = [x[0] for x in br.members]
-        y = [0 for v in x]
-        ax.scatter(x, y, marker='.')
-        ax.set_xlim(ax_lims[0])
+    def scatter_d1(self):
+        x = [x[0] for x in self.branching.members]
+        y = [0 for _v in x]
+        self.ax.scatter(x, y, marker='.')
+        self.ax.set_xlim(self.ax_lims[0])
         
-    def scatter_d2():
-        x, y = zip(*br.members)
-        ax.scatter(x, y, marker='.', s=10)
-        ax.set_xlim(ax_lims[0])
-        ax.set_ylim(ax_lims[1])
-        ax.annotate("Gen: {}".format(br.generation),
+    def scatter_d2(self):
+        x, y = zip(*self.branching.members)
+        self.ax.scatter(x, y, marker='.', s=10)
+        self.ax.set_xlim(self.ax_lims[0])
+        self.ax.set_ylim(self.ax_lims[1])
+        self.ax.annotate("Gen: {}".format(self.branching.generation),
             xy=(1, 0), xycoords='axes fraction',
             xytext=(-10, 10), textcoords='offset pixels',
             horizontalalignment='right',
             verticalalignment='bottom')
 
-    def scatter_d3():
-        x, y, z = zip(*br.members)
-        ax.scatter(x, y, z, s=5)
-        ax.set_xlim(ax_lims[0])
-        ax.set_ylim(ax_lims[1])
-        ax.set_zlim(ax_lims[2])
+    def scatter_d3(self):
+        x, y, z = zip(*self.branching.members)
+        self.ax.scatter(x, y, z, s=5)
+        self.ax.set_xlim(self.ax_lims[0])
+        self.ax.set_ylim(self.ax_lims[1])
+        self.ax.set_zlim(self.ax_lims[2])
 
-    scatter = [scatter_d1, scatter_d2, scatter_d3][ndim-1]
+    def setup_controls(self):
+        from matplotlib.widgets import Button, RadioButtons, Slider
+        self.g_branch_prob = Slider(plt.axes([0.1, 0.03, 0.1, 0.02]), 'Branch\nprob', 0, 1,
+                              valinit=self.branching.branch_prob, valfmt='%.2f')
+        self.g_branch_prob.on_changed(self.update_branch_prob)
+        self.g_max_size = Slider(plt.axes([0.3, 0.03, 0.1, 0.02]), 'Max\nsize', 1, 5,
+                              valinit=np.log10(self.branching.max_size), valfmt='%.0f')
+        self.g_max_size.valtext.set_text(self.branching.max_size)
+        self.g_max_size.on_changed(self.update_max_size)
+        self.g_fitness = RadioButtons(plt.axes([0.5, 0.01, 0.15, 0.13]), Branching.fitness_functions, self.branching.fitness)
+        self.g_fitness.on_clicked(self.update_fitness)
+        self.g_steps = Slider(plt.axes([0.8, 0.03, 0.1, 0.02]), 'Steps per\nupdate', 0, 3,
+                              valinit=np.log10(self.steps_per_update), valfmt='%.0f')
+        self.g_steps.valtext.set_text(self.steps_per_update)
+        self.g_steps.on_changed(self.update_steps)
+        self.g_tgl = Button(plt.axes([0.92, 0.1, 0.07, 0.04]), "Stop/Go")
+        self.g_tgl.on_clicked(self.toggle_running)
+        
+    def update_branch_prob(self, event):
+        self.branching.branch_prob = self.g_branch_prob.val
 
-    fig = plt.figure(figsize=(9, 8))
-    ax = plot()
+    def update_max_size(self, event):
+        self.branching.max_size = int(10**self.g_max_size.val)
+        self.g_max_size.valtext.set_text(self.branching.max_size)
 
-    class Controls(object): pass
-    controls = Controls()
+    def update_fitness(self, label):
+        self.branching.set_fitness(label)
 
-    class Controller(object):
-        def __init__(self):
-            self.running = True
-            self.steps = steps_per_update
-        def toggle_running(self, event):
-            if self.running:
-                ani.event_source.stop()
-            else:
-                ani.event_source.start()
-            self.running = not self.running
-        def update_steps(self, event):
-            controller.steps = int(controls.steps.val)
+    def update_steps(self, event):
+        self.steps_per_update = int(10**self.g_steps.val)
+        self.g_steps.valtext.set_text(self.steps_per_update)
 
-    controller = Controller()
+    def toggle_running(self, event):
+        if self.running:
+            self.ani.event_source.stop()
+        else:
+            self.ani.event_source.start()
+        self.running = not self.running
 
-    def update(i):
+    def update(self, i):
         import time        
         t = time.time()
-        br.run(controller.steps)
-        ax.clear()
-        scaling()
-        scatter()
-        print("{}, time: {:.2}".format(br.stats(), time.time() - t))
+        self.branching.run(self.steps_per_update)
+        self.ax.clear()
+        self.scaling()
+        self.scatter()
+        print("{}, time: {:.2}".format(self.branching.stats(), time.time() - t))
 
-    from matplotlib.widgets import Button, Slider
-    controls.tgl = Button(plt.axes([0.8, 0.02, 0.1, 0.04]), "Stop/Go")
-    controls.tgl.on_clicked(controller.toggle_running)
-    controls.steps = Slider(plt.axes([0.55, 0.03, 0.2, 0.02]), 'Steps', 1, 100, valinit=controller.steps, valfmt='%.0f')
-    controls.steps.on_changed(controller.update_steps)
-
-    ani = animation.FuncAnimation(fig, update, interval=1, init_func=lambda: None)
-
-    plt.show()
-
-euclidean_fitness = lambda gen: np.sqrt(np.sum(gen**2, axis=1))
-sumsq_fitness = lambda gen: np.sum(gen**2, axis=1)
-sumabs_fitness = lambda gen: np.sum(abs(gen), axis=1)
-maxabs_fitness = lambda gen: np.max(abs(gen), axis=1)
-absprod_fitness = lambda gen: np.prod(abs(gen), axis=1)
-
-def sparsity_fitness_fun(gen, radius):
-    from scipy import spatial
-    tree = spatial.cKDTree(gen)
-    f = lambda p: len(tree.query_ball_point(p, r=radius))
-    counts = np.apply_along_axis(f, 1, gen)
-    return counts
-sparsity_fitness_r50 = lambda gen: sparsity_fitness_fun(gen, 50), True
+    def run(self):
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval=1, init_func=lambda: None)
+        self.running = True
+        plt.show()
 
 ndim = 2
-simulation(Branching(branch_prob=0.05,
-                     fitness=euclidean_fitness,
-                     initial_population=np.zeros(shape=(1, ndim)),
-                     max_size=1000),
-           steps_per_update=1)
+simulation = Simulation(Branching(branch_prob=0.05,
+                                  fitness="euclidean",
+                                  initial_population=np.zeros(shape=(1, ndim)),
+                                  max_size=1000),
+                        steps_per_update=1)
+simulation.run()
